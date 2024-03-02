523bf2cd8aba84ecb75d7ed9abfd3fb7a48c938e
kimiaBadri <142110633+kimiaBadri@users.noreply.github.com>
2024-03-02 10:50:42 +0330

    creaye bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:digi_kala_project/constants/colors.dart';
 import 'package:digi_kala_project/screens/product_list_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +14,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(),
-          body: ProductListScreen(),
-          bottomNavigationBar: BottomNavigationBar(items: []),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: CustomColor.backGroundScreen,
+        appBar: AppBar(),
+        body: ProductListScreen(),
+        bottomNavigationBar: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Container(
+                    child: Image.asset('assets/images/icon_home.png'),
+                    decoration: BoxDecoration(boxShadow: [
+                      BoxShadow(
+                          color: CustomColor.blue,
+                          blurRadius: 20,
+                          spreadRadius: -7,
+                          offset: Offset(0.0, 13))
+                    ]),
+                  ),
+                  label: 'home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset('assets/images/icon_home.png'),
+                  label: 'home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset('assets/images/icon_home.png'),
+                  label: 'home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset('assets/images/icon_home.png'),
+                  label: 'home',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
